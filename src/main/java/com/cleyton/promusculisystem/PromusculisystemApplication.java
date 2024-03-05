d7409fe3e7e855a3d99e0af97f78d12865d2d257
Cleyton Souza Martins <cleytonsm1309@gmail.com>
2024-03-05 12:21:58 -0300

    First Commit

---
+++ src/main/java/com/cleyton/promusculisystem/PromusculisystemApplication.java
@@ -0,0 +1,13 @@
+package com.cleyton.promusculisystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromusculisystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromusculisystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cleyton/promusculisystem/PromusculisystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleyton.promusculisystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromusculisystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
