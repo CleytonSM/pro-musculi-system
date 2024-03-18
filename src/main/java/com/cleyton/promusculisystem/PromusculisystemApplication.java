0f6b0905f7d499737b8b114c2051123099c726b9
Cleyton Souza Martins <cleytonsm1309@gmail.com>
2024-03-18 15:26:38 -0300

    Add new login route

--- src/main/java/com/cleyton/promusculisystem/PromusculisystemApplication.java
+++ src/main/java/com/cleyton/promusculisystem/PromusculisystemApplication.java
@@ -2,8 +2,6 @@ package com.cleyton.promusculisystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PromusculisystemApplication {
