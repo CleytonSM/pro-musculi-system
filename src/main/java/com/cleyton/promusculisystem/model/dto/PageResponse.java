1a175804f3cc174de851a4eda0a706544e5ed9e4
Cleyton Souza Martins <cleytonsm1309@gmail.com>
2024-03-27 17:56:25 -0300

    Add PageResponse as reponse

---
+++ src/main/java/com/cleyton/promusculisystem/model/dto/PageResponse.java
@@ -0,0 +1,12 @@
+package com.cleyton.promusculisystem.model.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class PageResponse<T> {
+    //private PaginationDto pagination;
+    private long total;
+    private List<T> record;
+}
