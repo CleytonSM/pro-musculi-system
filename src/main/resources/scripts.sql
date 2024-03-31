d06f0f7319933ef09ffd9e19f4c482d37bbbe9c5
Cleyton Souza Martins <cleytonsm1309@gmail.com>
2024-03-31 14:16:27 -0300

    Add GymPlan entity

--- src/main/resources/scripts.sql
+++ src/main/resources/scripts.sql
@@ -12,3 +12,24 @@ CREATE TABLE tb_authorities (
                                 user_id INTEGER,
                                 CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES tb_users(id)
 );
+
+CREATE TABLE tb_clients (
+                            id INTEGER AUTO_INCREMENT,
+                            name VARCHAR(60) NOT NULL,
+                            email VARCHAR(100) NOT NULL UNIQUE,
+                            phone VARCHAR(11) UNIQUE,
+                            active TINYINT NOT NULL DEFAULT(1),
+                            created_at TIMESTAMP,
+                            CONSTRAINT pk_tb_clients_id PRIMARY KEY (id)
+);
+
+CREATE TABLE tb_gym_plan (
+                             id INTEGER AUTO_INCREMENT,
+                             client_id INTEGER NOT NULL,
+                             name VARCHAR(40) NOT NULL,
+                             price DECIMAL(5,2) NOT NULL,
+                             expire_at TIMESTAMP,
+                             created_at TIMESTAMP,
+                             CONSTRAINT pk_tb_gym_plan_id PRIMARY KEY (id),
+                             CONSTRAINT fk_tb_gym_plan_client_id FOREIGN KEY (client_id) REFERENCES tb_clients(id)
+);
