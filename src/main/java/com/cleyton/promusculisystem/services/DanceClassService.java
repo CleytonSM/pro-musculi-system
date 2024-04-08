5e202e40ad138cad56d26a6f80e668ccaca812b6
Cleyton Souza Martins <cleytonsm1309@gmail.com>
2024-04-08 14:24:33 -0300

    Add dance class controller

---
+++ src/main/java/com/cleyton/promusculisystem/services/DanceClassService.java
@@ -0,0 +1,11 @@
+package com.cleyton.promusculisystem.services;
+
+import com.cleyton.promusculisystem.model.dto.DanceClassDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DanceClassService {
+    public void createDanceClass(DanceClassDto danceClassDto) {
+
+    }
+}
