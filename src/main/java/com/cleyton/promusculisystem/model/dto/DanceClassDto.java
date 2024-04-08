60f339d5da8d986a8b5cb4f07763e738ff45b619
Cleyton Souza Martins <cleytonsm1309@gmail.com>
2024-04-08 14:24:33 -0300

    Add dance class controller

---
+++ src/main/java/com/cleyton/promusculisystem/model/dto/DanceClassDto.java
@@ -0,0 +1,13 @@
+package com.cleyton.promusculisystem.model.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class DanceClassDto {
+    private String clientName;
+    private String instructorName;
+    private String name;
+    private LocalDateTime dateTime;
+}
