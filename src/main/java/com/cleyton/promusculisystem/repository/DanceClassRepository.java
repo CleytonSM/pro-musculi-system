6c5b2ced8e53fa0b921d190aee376318d6839b72
Cleyton Souza Martins <cleytonsm1309@gmail.com>
2024-04-22 11:05:32 -0300

    Add GET methods on DanceClass routes

--- src/main/java/com/cleyton/promusculisystem/repository/DanceClassRepository.java
+++ src/main/java/com/cleyton/promusculisystem/repository/DanceClassRepository.java
@@ -1,7 +1,11 @@
 package com.cleyton.promusculisystem.repository;
 
 import com.cleyton.promusculisystem.model.DanceClass;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -11,5 +15,7 @@ import java.util.Optional;
 public interface DanceClassRepository extends JpaRepository<DanceClass, Integer> {
 
     Optional<DanceClass> findByStartAndEnd(LocalDateTime start, LocalDateTime end);
-
+    Optional<DanceClass> findByName(String name);
+    @Query("SELECT dc FROM DanceClass dc WHERE dc.user.email = :email")
+    Page<DanceClass> findAllByUserEmail(@Param("email") String email, Pageable pageable);
 }
