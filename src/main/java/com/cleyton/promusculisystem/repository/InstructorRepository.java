dd318812712b21cacaf951c2b3db85244066c011
Cleyton Souza Martins <cleytonsm1309@gmail.com>
2024-04-28 11:54:48 -0300

    Add instructor finder in InstructorController

--- src/main/java/com/cleyton/promusculisystem/repository/InstructorRepository.java
+++ src/main/java/com/cleyton/promusculisystem/repository/InstructorRepository.java
@@ -2,6 +2,8 @@ package com.cleyton.promusculisystem.repository;
 
 import com.cleyton.promusculisystem.model.Instructor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,5 +11,6 @@ import java.util.Optional;
 @Repository
 public interface InstructorRepository extends JpaRepository<Instructor, Integer> {
 
-    Optional<Object> findByCpf(String cpf);
+    @Query("SELECT i FROM Instructor i WHERE i.cpf = :cpf AND i.active = TRUE")
+    Optional<Instructor> findByCpf(@Param("cpf") String cpf);
 }
