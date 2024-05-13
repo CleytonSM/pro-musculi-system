716a1f8ed8cc4bd521de5dd2dce860c3d4363a7b
Cleyton Souza Martins <cleytonsm1309@gmail.com>
2024-05-13 11:16:01 -0300

    Add WorkoutClass as ORM entity

--- src/main/java/com/cleyton/promusculisystem/model/WorkoutClass.java
+++ src/main/java/com/cleyton/promusculisystem/model/WorkoutClass.java
@@ -1,6 +1,5 @@
 package com.cleyton.promusculisystem.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -9,43 +8,33 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
 
 import java.time.LocalDateTime;
-import java.util.Set;
 
 @Entity
 @Data
-@Table(name = "tb_clients")
-public class Client {
-
+@Table(name = "tb_workout_classes")
+public class WorkoutClass {
     @Id
     @GeneratedValue(generator = "native", strategy = GenerationType.AUTO)
     @GenericGenerator(name = "native")
-    @JsonIgnore
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Integer id;
     @ManyToOne
-    @JoinColumn(name = "gym_plan_id")
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    private GymPlan gymPlan;
-    @Column(nullable = false, length = 60)
+    @JoinColumn(name = "client_id")
+    private Client client;
+    @ManyToOne
+    @JoinColumn(name = "instructor_id")
+    private Instructor instructor;
+    @Column(length = 60)
     private String name;
-    @Column(length = 100, unique = true)
-    private String email;
-    @Column(length = 11, unique = true)
-    private String phone;
-    @Column(nullable = false)
-    @JsonIgnore
+    @Column(name = "dt_class")
+    private LocalDateTime dateClass;
     private Boolean active;
+    @Column(length = 100)
+    private String description;
     private LocalDateTime createdAt;
-
-    @OneToMany(mappedBy = "client")
-    @JsonIgnore
-    private Set<DanceClass> danceClasses;
-    @OneToMany(mappedBy = "client")
-    @JsonIgnore
-    private Set<WorkoutClass> workoutClasses;
 }
