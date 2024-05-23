aac737308a70d8203c261ccca6064e5fd82d8e32
Cleyton Souza Martins <cleytonsm1309@gmail.com>
2024-05-23 19:54:39 -0300

    Add '@Repository' above repository interfaces

--- src/main/java/com/cleyton/promusculisystem/repository/GymPlanRepository.java
+++ src/main/java/com/cleyton/promusculisystem/repository/GymPlanRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface GymPlanRepository extends JpaRepository<GymPlan, Integer> {
 
     Optional<GymPlan> findByName(String name);
