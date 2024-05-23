bab80a15c5f614e29da622a8c9325f815ef54b6f
Cleyton Souza Martins <cleytonsm1309@gmail.com>
2024-05-23 19:54:39 -0300

    Add '@Repository' above repository interfaces

--- src/main/java/com/cleyton/promusculisystem/repository/AuthorityRepository.java
+++ src/main/java/com/cleyton/promusculisystem/repository/AuthorityRepository.java
@@ -4,9 +4,11 @@ import com.cleyton.promusculisystem.model.Authority;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface AuthorityRepository extends JpaRepository<Authority, Integer> {
 
     @Query("SELECT a FROM Authority a WHERE a.user.id = :id")
--- src/main/java/com/cleyton/promusculisystem/repository/UserRepository.java
+++ src/main/java/com/cleyton/promusculisystem/repository/UserRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
 
