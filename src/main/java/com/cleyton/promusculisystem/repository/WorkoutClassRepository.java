2c94e1c1aeac19476fcd5061c4ef73869e5c3024
Cleyton Souza Martins <cleytonsm1309@gmail.com>
2024-05-23 19:58:15 -0300

    Add '@Repository' above repository interfaces

--- src/main/java/com/cleyton/promusculisystem/repository/WorkoutClassRepository.java
+++ src/main/java/com/cleyton/promusculisystem/repository/WorkoutClassRepository.java
@@ -19,4 +19,3 @@ public interface WorkoutClassRepository extends JpaRepository<WorkoutClass, Inte
     @Query("SELECT wc FROM WorkoutClass wc WHERE wc.active = FALSE")
     Optional<WorkoutClass> findInactiveById(Integer id);
 }
-
