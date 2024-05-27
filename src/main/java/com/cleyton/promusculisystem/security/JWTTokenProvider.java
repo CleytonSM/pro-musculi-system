76ed7e3b9c39482859484d7ae85a99a276e9fb86
Cleyton Souza Martins <cleytonsm1309@gmail.com>
2024-05-27 17:05:33 -0300

    Add getAuthentication

--- src/main/java/com/cleyton/promusculisystem/security/JWTTokenProvider.java
+++ src/main/java/com/cleyton/promusculisystem/security/JWTTokenProvider.java
@@ -1,21 +1,39 @@
 package com.cleyton.promusculisystem.security;
 
+import com.cleyton.promusculisystem.helper.SecretKeyHelper;
+import com.cleyton.promusculisystem.model.Authority;
 import com.cleyton.promusculisystem.model.User;
 import com.cleyton.promusculisystem.model.dto.UserDTO;
+import com.cleyton.promusculisystem.repository.UserRepository;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.JwtParserBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.SecretKeyFactorySpi;
 import java.nio.charset.StandardCharsets;
+import java.security.Provider;
+import java.util.ArrayList;
 import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+
+import static com.cleyton.promusculisystem.helper.ModelAttributeSetterHelper.verifyOptionalEntity;
 
 @Component
 public class JWTTokenProvider {
@@ -26,6 +44,9 @@ public class JWTTokenProvider {
     @Value("${security.jwt.token.expire-length}")
     private long validityInMilliseconds;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @PostConstruct
     protected void init() {
         secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
@@ -33,9 +54,10 @@ public class JWTTokenProvider {
 
     public String createToken(String email, UserDTO userDTO, User user) {
         Date now = new Date();
-        Date validity = validitySetUp(now);
+        SecretKeyHelper secretKeyHelper = new SecretKeyHelper();
 
-        SecretKey key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+        Date validity = validitySetUp(now);
+        SecretKey key = secretKeyHelper.secretKeyBuilder(secretKey);
 
         return Jwts.builder()
                 .issuer("ProMusculiSystem")
@@ -67,5 +89,28 @@ public class JWTTokenProvider {
 
     public Date validitySetUp(Date now) {
         return new Date(now.getTime() + validityInMilliseconds);
+    }
+
+    public boolean validateToken(String token) {
+
+    }
+
+    public String getUsername(String token) {
+        SecretKeyHelper secretKeyHelper = new SecretKeyHelper();
+
+        SecretKey key = secretKeyHelper.secretKeyBuilder(secretKey);
+
+        return Jwts.parser().verifyWith(key)
+    }
+
+    public Authentication getAuthentication(String token) {
+        User user = verifyOptionalEntity(userRepository.findByEmail(getUsername(token)));
+
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+
+        for (Authority authority : user.getAuthorities()) {
+            grantedAuthorities.add(new SimpleGrantedAuthority(authority.getName()));
+        }
+        return new UsernamePasswordAuthenticationToken(user, "", grantedAuthorities);
     }
 }
