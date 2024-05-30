f23a35d1ebded4b703c9ebcd51733e424938d787
Cleyton Souza Martins <cleytonsm1309@gmail.com>
2024-05-30 15:02:23 -0300

    Add changes

---
+++ src/main/java/com/cleyton/promusculisystem/model/response/LoginResponseDTO.java
@@ -0,0 +1,9 @@
+package com.cleyton.promusculisystem.model.response;
+
+public class LoginResponseDTO {
+    private String token;
+
+    public LoginResponseDTO(String token) {
+        this.token = token;
+    }
+}
