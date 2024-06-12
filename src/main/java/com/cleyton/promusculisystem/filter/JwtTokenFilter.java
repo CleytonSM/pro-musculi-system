616390160da65734d7e60f890776b176bf7c94ce
Cleyton Souza Martins <cleytonsm1309@gmail.com>
2024-06-12 09:23:02 -0300

    feat: Change login from GetMapping to PostMapping

---
+++ src/main/java/com/cleyton/promusculisystem/filter/JwtTokenFilter.java
@@ -0,0 +1,41 @@
+package com.cleyton.promusculisystem.filter;
+
+import com.cleyton.promusculisystem.security.JwtTokenProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+
+public class JwtTokenFilter extends GenericFilterBean {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtTokenFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+            throws IOException, ServletException {
+        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+        try {
+            if (token != null && jwtTokenProvider.validateToken(token)) {
+                Authentication auth = jwtTokenProvider.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        } catch (Exception ex) {
+            HttpServletResponse errorResponse = (HttpServletResponse) response;
+            errorResponse.sendError(401, ex.getMessage());
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
