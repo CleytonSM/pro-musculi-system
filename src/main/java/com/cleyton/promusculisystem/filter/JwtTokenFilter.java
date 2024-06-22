48e04533e16d4b2d254a61bcb2d4760c20de7224
Cleyton Souza Martins <cleytonsm1309@gmail.com>
2024-06-22 17:04:33 -0300

    feat: change filter logic

--- src/main/java/com/cleyton/promusculisystem/filter/JwtTokenFilter.java
+++ src/main/java/com/cleyton/promusculisystem/filter/JwtTokenFilter.java
@@ -3,39 +3,40 @@ package com.cleyton.promusculisystem.filter;
 import com.cleyton.promusculisystem.security.JwtTokenProvider;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-public class JwtTokenFilter extends GenericFilterBean {
+public class JwtTokenFilter extends OncePerRequestFilter {
 
     private final JwtTokenProvider jwtTokenProvider;
 
-    public JwtTokenFilter(JwtTokenProvider jwtTokenProvider) {
-        this.jwtTokenProvider = jwtTokenProvider;
+    public JwtTokenFilter() {
+        this.jwtTokenProvider = new JwtTokenProvider();
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
-            throws IOException, ServletException {
-        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = jwtTokenProvider.resolveToken(request);
         try {
             if (token != null && jwtTokenProvider.validateToken(token)) {
                 Authentication auth = jwtTokenProvider.getAuthentication(token);
                 SecurityContextHolder.getContext().setAuthentication(auth);
             }
         } catch (Exception ex) {
-            HttpServletResponse errorResponse = (HttpServletResponse) response;
-            errorResponse.sendError(401, ex.getMessage());
+            response.sendError(401, ex.getMessage());
             return;
         }
 
         filterChain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        return request.getRequestURI().equals("/user/auth");
     }
 }
--- src/main/java/com/cleyton/promusculisystem/filter/JwtTokenValidatorFilter.java
+++ src/main/java/com/cleyton/promusculisystem/filter/JwtTokenValidatorFilter.java
@@ -1,60 +1,60 @@
-package com.cleyton.promusculisystem.filter;
-
-import com.cleyton.promusculisystem.exceptions.InvalidJWTToken;
-import com.cleyton.promusculisystem.helper.SecretKeyHelper;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import javax.crypto.SecretKey;
-import java.io.IOException;
-
-public class JwtTokenValidatorFilter extends OncePerRequestFilter {
-
-    @Value("${security.jwt.token.secret-key}")
-    private String secretKey;
-
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
-        String jwt = request.getHeader("Authorization");
-        // TODO resolve token here?
-        if(jwt != null) {
-            try {
-                SecretKeyHelper secretKeyHelper = new SecretKeyHelper();
-                SecretKey key = secretKeyHelper.secretKeyBuilder(secretKey);
-
-                Claims claims = Jwts.parser()
-                        .decryptWith(key)
-                        .build()
-                        .parseUnsecuredClaims(jwt)
-                        .getPayload();
-
-                String username = String.valueOf(claims.get("email"));
-                String authorities = (String) claims.get("authorities");
-
-                Authentication authentication = new UsernamePasswordAuthenticationToken(username,
-                        null, AuthorityUtils.commaSeparatedStringToAuthorityList(authorities));
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-            } catch (Exception e) {
-                throw new InvalidJWTToken("Invalid Token received!");
-            }
-        }
-
-        filterChain.doFilter(request, response);
-    }
-
-    @Override
-    protected boolean shouldNotFilter(HttpServletRequest request) {
-        return request.getServletPath().equals("/user/auth");
-    }
-}
+//package com.cleyton.promusculisystem.filter;
+//
+//import com.cleyton.promusculisystem.exceptions.InvalidJWTToken;
+//import com.cleyton.promusculisystem.helper.SecretKeyHelper;
+//import io.jsonwebtoken.Claims;
+//import io.jsonwebtoken.Jwts;
+//import jakarta.servlet.FilterChain;
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.authority.AuthorityUtils;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.web.filter.OncePerRequestFilter;
+//
+//import javax.crypto.SecretKey;
+//import java.io.IOException;
+//
+//public class JwtTokenValidatorFilter extends OncePerRequestFilter {
+//
+//    @Value("${security.jwt.token.secret-key}")
+//    private String secretKey;
+//
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+//            throws ServletException, IOException {
+//        String jwt = request.getHeader("Authorization");
+//        // TODO resolve token here?
+//        if(jwt != null) {
+//            try {
+//                SecretKeyHelper secretKeyHelper = new SecretKeyHelper();
+//                SecretKey key = secretKeyHelper.secretKeyBuilder(secretKey);
+//
+//                Claims claims = Jwts.parser()
+//                        .decryptWith(key)
+//                        .build()
+//                        .parseUnsecuredClaims(jwt)
+//                        .getPayload();
+//
+//                String username = String.valueOf(claims.get("email"));
+//                String authorities = (String) claims.get("authorities");
+//
+//                Authentication authentication = new UsernamePasswordAuthenticationToken(username,
+//                        null, AuthorityUtils.commaSeparatedStringToAuthorityList(authorities));
+//                SecurityContextHolder.getContext().setAuthentication(authentication);
+//            } catch (Exception e) {
+//                throw new InvalidJWTToken("Invalid Token received!");
+//            }
+//        }
+//
+//        filterChain.doFilter(request, response);
+//    }
+//
+//    @Override
+//    protected boolean shouldNotFilter(HttpServletRequest request) {
+//        return request.getServletPath().equals("/user/auth");
+//    }
+//}
