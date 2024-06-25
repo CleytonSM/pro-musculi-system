a5cee18f78d59cc3fabc6457c79b43c3f8d032b8
CleytonSM <cleytonsm1309@gmail.com>
2024-06-25 20:53:52 -0300

    add jwt validation

--- src/main/java/com/cleyton/promusculisystem/filter/JwtTokenFilter.java
+++ src/main/java/com/cleyton/promusculisystem/filter/JwtTokenFilter.java
@@ -5,19 +5,21 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.cdi.Eager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
+@Eager
 public class JwtTokenFilter extends OncePerRequestFilter {
 
-    private final JwtTokenProvider jwtTokenProvider;
-
-    public JwtTokenFilter() {
-        this.jwtTokenProvider = new JwtTokenProvider();
-    }
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
